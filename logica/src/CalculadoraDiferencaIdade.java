d543ccb9cbcb608dc117b8f28360fad88d196f22
JotaP53 <martinsjp53@gmail.com>
2024-06-22 16:07:26 -0300

    updating repository

---
+++ logica/src/CalculadoraDiferencaIdade.java
@@ -0,0 +1,18 @@
+/*
+Crie uma classe chamada CalculadoraDiferencaIdade.
+Crie uma variável para guardar a primeira idade.
+Crie uma variável para guardar a segunda idade.
+Crei uma variável para guardar a diferença entre a idade 1 e a idade 2.
+Imprima a diferença no console.
+*/
+
+public class CalculadoraDiferencaIdade {
+    public static void main(String[] args) {
+
+        int age1 = 26;
+        int age2 = 35;
+        int ageDifference = age1 - age2;
+
+        System.out.println(ageDifference);
+    }
+}
