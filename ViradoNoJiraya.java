b8ac82e6ddbf1f475640cb35c848846e45a03d43
JotaP53 <martinsjp53@gmail.com>
2024-06-22 09:23:52 -0300

    primeiro commit

---
+++ ViradoNoJiraya.java
@@ -0,0 +1,19 @@
+// No Java, quando está se declarando um classe,
+// a primeira letra será maiúscula.
+public class ViradoNoJiraya {
+
+	// O Java sempre tem um começo.
+	// Ele começará a ser executado a partir daqui.
+	// Diferente das classes, os métodos começam com letras minúsculas.
+	// Se o método tiver mais de uma palavra, ele seguirá camelCase.
+	// Ex.: mainExemploDois
+	public static void main(String[] args){
+		
+		// Esta linha não é um método, é uma linha de execução.
+		// Deve-se colocar ";" para sinalizar que o comando terminou.
+		System.out.println("Hello, World!");
+	}
+}
+
+// Sempre salve o arquivo com o nome da classe.
+// Neste caso: ViradoNoJiraya.
