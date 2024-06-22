36abaf0b0084c1418c844dcc6655fe59e055eb09
JotaP53 <martinsjp53@gmail.com>
2024-06-22 13:29:55 -0300

    updating repository

---
+++ CalculadoraDiferencaIdade.java
@@ -0,0 +1,18 @@
+/*
+Crie uma classe chamada CalculadoraDiferencaIdade.
+Crie uma variável para guardar a primeira idade.
+Crie uma variável para guardar a segunda idade.
+Crei uma variável para guardar a diferença entre a idade 1 e a idade 2.
+Imprima a diferença no console.
+*/
+
+public class CalculadoraDiferencaIdade {
+	public static void main(String[] args) {
+
+		int idade1 = 26;
+		int idade2 = 35;
+		int difIdade = idade1 - idade2;
+
+		System.out.println(difIdade);
+	}
+}
