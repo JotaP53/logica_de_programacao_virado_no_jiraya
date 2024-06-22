e6bff84099270811f603005ef5ab88be52f43740
JotaP53 <martinsjp53@gmail.com>
2024-06-22 13:31:12 -0300

    updating repository

--- VariaveisETiposPrimitivos.java
+++ VariaveisETiposPrimitivos.java
@@ -1,23 +1,33 @@
-// No Java, quando está se declarando um classe, a primeira letra será maiúscula.
+/*
+No Java, quando está se declarando um classe, a primeira letra será maiúscula.
+*/
 public class VariaveisETiposPrimitivos {
 
-	// O Java sempre tem um começo.
-	// Ele começará a ser executado a partir daqui.
-	// Diferente das classes, os métodos começam com letras minúsculas.
-	// Se o método tiver mais de uma palavra, ele seguirá camelCase.
-	// Ex.: mainExemploDois
+	/*
+	O Java sempre tem um começo.
+	Ele começará a ser executado a partir daqui.
+	Diferente das classes, os métodos começam com letras minúsculas.
+	Se o método tiver mais de uma palavra, ele seguirá camelCase.
+	Ex.: mainExemploDois
+	*/
 	public static void main(String[] args){
 
-		// Se a variável tiver mais de uma palavra, ele seguirá camelCase.
-		// Ex.: idadeDoAlunoDois
+		/*
+		Se a variável tiver mais de uma palavra, ele seguirá camelCase.
+		Ex.: idadeDoAlunoDois
+		*/
 		int idade = 26;
 		
-		// Esta é uma linha de execução.
-		// Deve-se colocar ";" para sinalizar que o comando terminou.
-		// Sempre que quiser juntar uma string com um valor que está dentro da memória precisa utilizar o "+".
+		/*
+		Esta é uma linha de execução.
+		Deve-se colocar ";" para sinalizar que o comando terminou.
+		Sempre que quiser juntar uma string com um valor que está dentro da memória precisa utilizar o "+".
+		*/
 		System.out.println("Idade = " + idade);
 	}
 }
 
-// Sempre salve o arquivo com o nome da classe.
-// Neste caso: ViradoNoJiraya.
+/*
+Sempre salve o arquivo com o nome da classe.
+Neste caso: ViradoNoJiraya.
+*/
